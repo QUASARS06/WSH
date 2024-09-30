9d63ad0194ac3309bdf0ea7c292897d6ff6dfd7e
Chirag Jain <chiragjain@Chirags-MBP.lan>
2024-09-30 18:37:04 -0500

    fixed history issues and implemented ls

--- solution/wsh.c
+++ solution/wsh.c
@@ -2,10 +2,43 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <dirent.h>
 #include "wsh.h"
 
+/**
+ * Reads input from stdin and then stores it in the cmd_buf passed
+ */
+int read_cmd(char *cmd_buf, size_t cmd_buf_sz) {
+    printf("wsh> ");
+
+    int cmdLength = getline(&cmd_buf, &cmd_buf_sz, stdin);
+    if(cmdLength == -1) return -1;
+
+    return 0;
+}
+
+/**
+ * Parses the cmd_buf string and breaks it into tokens separated by " "
+ * The tokens are then saved in the cmg_args_list array
+ */
+int parse_cmd(char *cmd_buf, char *cmd_args_list[]) {
+    char *token;
+
+    // first token
+    token = strtok(cmd_buf, " ");
+
+    int i = 0;
+    while(token != NULL) {
+        cmd_args_list[i] = token;
+        i++;
+        token = strtok(NULL, " ");
+    }
+    cmd_args_list[i] = NULL;
+
+    return 0;
+}
+
 void printHistory(HistNode *HEAD) {
-    
     HistNode *ptr = HEAD;
     
     int i = 1;
@@ -14,6 +47,16 @@ void printHistory(HistNode *HEAD) {
         i++;
         ptr = ptr->next;
     }
+}
+
+char * searchHistory(HistNode *HEAD, int target_idx) {
+    HistNode *ptr = HEAD;
+    while(target_idx > 1) {
+        ptr = ptr->next;
+        target_idx--;
+    }
+
+    return ptr->cmd;
 }
 
 void addToHistory(HistNode **HEAD, HistNode **TAIL, char* cmd, int *curr_hist_size, int hist_capacity) {
@@ -26,6 +69,7 @@ void addToHistory(HistNode **HEAD, HistNode **TAIL, char* cmd, int *curr_hist_si
     if(*HEAD == NULL && *TAIL == NULL) {
         *HEAD = NN;
         *TAIL = NN;
+        *curr_hist_size += 1;
         return;
     }
 
@@ -45,7 +89,7 @@ void addToHistory(HistNode **HEAD, HistNode **TAIL, char* cmd, int *curr_hist_si
 
 }
 
-void updateHistoryCapacity(HistNode *HEAD, HistNode *TAIL, int *curr_hist_size, int *old_hist_capacity, int new_hist_capacity) {
+void updateHistoryCapacity(HistNode **HEAD, HistNode **TAIL, int *curr_hist_size, int *old_hist_capacity, int new_hist_capacity) {
     
     // (I) if we are shrinking the capacity then we need to think about different cases
     // Case 1: curr_hist_size >= new_hist_size - means we have more history then we can fit so delete
@@ -57,10 +101,10 @@ void updateHistoryCapacity(HistNode *HEAD, HistNode *TAIL, int *curr_hist_size, 
     
     // (II) if the new capacity is greater than the old capacity then just update the hist_capacity variable
     // (III) if the new and old hist capacities are the same then don't do anything
-    (void)HEAD;
+    
     if(new_hist_capacity == 0) {
-        HEAD = NULL;
-        TAIL = NULL;
+        *HEAD = NULL;
+        *TAIL = NULL;
         *old_hist_capacity = new_hist_capacity;
         *curr_hist_size = 0;
     }
@@ -70,9 +114,9 @@ void updateHistoryCapacity(HistNode *HEAD, HistNode *TAIL, int *curr_hist_size, 
         int nodesToDelete = *curr_hist_size - new_hist_capacity;
 
         while(nodesToDelete > 0) {
-            HistNode *t = TAIL;
-            TAIL = TAIL->prev;
-            TAIL->next = NULL;
+            HistNode *t = *TAIL;
+            *TAIL = (*TAIL)->prev;
+            (*TAIL)->next = NULL;
 
             free(t);
             nodesToDelete -= 1;
@@ -83,37 +127,74 @@ void updateHistoryCapacity(HistNode *HEAD, HistNode *TAIL, int *curr_hist_size, 
     *old_hist_capacity = new_hist_capacity;
 }
 
-int read_cmd(char *cmd_buf, size_t cmd_buf_sz) {
-    printf("wsh> ");
+int history(char *cmd_args[], HistNode **histHead, HistNode **histTail, bool *is_from_history, int *curr_hist_size, int *hist_capacity) {
 
-    int cmdLength = getline(&cmd_buf, &cmd_buf_sz, stdin);
-    if(cmdLength == -1) return -1;
-
-    return 0;
-}
-
-int parse_cmd(char *cmd_buf, char *cmd_args_list[]) {
-    char *token;
-
-    // first token
-    token = strtok(cmd_buf, " ");
-
-    int i = 0;
-    while(token != NULL) {
-        cmd_args_list[i] = token;
-        i++;
-        token = strtok(NULL, " ");
+    // Built-In history command
+    if(cmd_args[1] == NULL) {
+        // print the history
+        printHistory(*histHead);
     }
-    cmd_args_list[i] = NULL;
+
+    // if history set # command is executed - update history size
+    else if(strcmp(cmd_args[1], "set") == 0) {
+        int new_hist_capactiy = atoi(cmd_args[2]);
+        if(new_hist_capactiy > 0) {
+            updateHistoryCapacity(histHead, histTail, curr_hist_size, hist_capacity, new_hist_capactiy);
+        }
+    }
+
+    else {
+        // check cmd_args[1] is a valid integer
+        *is_from_history = true;
+        int hist_idx = atoi(cmd_args[1]);
+        if(hist_idx > 0 && hist_idx <= *curr_hist_size) {
+            char *hist_cmd = searchHistory(*histHead, hist_idx);
+            printf("%s\n", hist_cmd);
+        }
+    }
 
     return 0;
 }
 
+int ls(char* path) {
+    if(path == NULL) {
+        path = ".";
+    }
 
-int exec_cmd(char *cmd, char *cmd_args[], HistNode **histHead, HistNode **histTail, int *curr_hist_size, int *hist_capacity) {
+    struct dirent **allFileNames; 
+    int n = scandir(path, &allFileNames, NULL, alphasort);
+
+    if(n < 0) {
+        perror("ls error");
+    } else {  
+        int i = 0;  
+        while(i < n) {
+            if(allFileNames[i]->d_name[0] != '.') {
+                printf("%s\n", allFileNames[i]->d_name);
+            }
+            free(allFileNames[i]);
+            i++;
+        }
+        free(allFileNames);
+    }
+
+    return 0;
+}
+
+int run_command(char *cmd, char *cmd_args[]) {
+    (void)cmd_args;
+    (void)cmd;
+    char *path = getenv("PATH");
+    printf("PATH = |%s|\n", path);
+    return 0;
+}
+
+
+int exec_cmd(char *cmd, char *cmd_args[], HistNode **histHead, HistNode **histTail, int *curr_hist_size, int *hist_capacity, char *last_command) {
 
     bool is_from_history = false;   // stores whether a NON built-in command is requested via history or not
-    
+    bool is_built_in = true;
+
     if(strcmp(cmd_args[0], "cd") == 0) {
         // Built-In change directory
     }
@@ -131,24 +212,28 @@ int exec_cmd(char *cmd, char *cmd_args[], HistNode **histHead, HistNode **histTa
     }
     else if(strcmp(cmd_args[0], "history") == 0) {
         // Built-In history command
-        if(cmd_args[1] == NULL) {
-            // print the history
-            printHistory(*histHead);
-        }
-
-        // if history # command is executed
-        if(strcmp(cmd_args[1], "set") != 0) {
-            is_from_history = true;
-        }
+        history(cmd_args, histHead, histTail, &is_from_history, curr_hist_size, hist_capacity);
     }
     else if(strcmp(cmd_args[0], "ls") == 0) {
         // Built-In ls -1 command
-        
+        ls(".");
     }
     else {
+        is_built_in = false;
         // fork and execute in child process
-        if(!is_from_history) addToHistory(histHead, histTail, cmd, curr_hist_size, *hist_capacity);
-        // since it's not a built-in command it will be saved in the history
+    }
+
+    // since it's not a built-in command it will be saved in the history
+    if(!is_from_history && !is_built_in && !(last_command != NULL && strcmp(last_command, cmd) == 0)) {
+        addToHistory(histHead, histTail, cmd, curr_hist_size, *hist_capacity);
+    }
+
+    // we set the current command being parsed always so that we can use it to update the history quickly
+    if(!is_built_in) {
+
+        //run_command(cmd, cmd_args);
+
+        strcpy(last_command, cmd);
     }
 
     return 0;
@@ -175,10 +260,10 @@ int main(int argc, char* argv[]) {
     char *cmd_args_list[MAXARGS];
 
     // stores the last command issued by the user
-    char *last_command = NULL;
+    char last_command[MAXLINE];
 
     while(read_cmd(cmd_buf, sizeof(cmd_buf)) >= 0) {
-        if(cmd_buf[strlen(cmd_buf) - 1] == '\n') {
+        if(cmd_buf[strlen(cmd_buf) - 1] == EOF || cmd_buf[strlen(cmd_buf) - 1] == '\n') {
             cmd_buf[strlen(cmd_buf) - 1] = '\0';
         }
 
@@ -191,16 +276,13 @@ int main(int argc, char* argv[]) {
 
 
         // check if built-in
-        exec_cmd(cmd_buf, cmd_args_list, &histHead, &histTail, &curr_history_size, &history_capacity);
+        exec_cmd(cmd_buf, cmd_args_list, &histHead, &histTail, &curr_history_size, &history_capacity, last_command);
 
-
-        // we set the current command being parsed always so that we can use it to update the history quickly
-        last_command = cmd_args_list[0];
     }
 
     // free(cmd_buf);
     // free(cmd_args_list);
 
-    free(last_command);
+    //free(last_command);
     return 0;
 }
